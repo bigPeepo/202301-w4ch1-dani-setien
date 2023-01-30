38fa3a25233bf18674b5f29081f5d1bf2fcc2477
Dani Setien <dani.setien@gmail.com>
2023-01-30 22:57:07 +0100

    Circumstancial: end of work day

--- components.md
+++ components.md
@@ -1,9 +1,49 @@
+# Data
+
+## Data layer
+
+- List of Gentlemen
+  - id
+  - name
+  - profession
+  - twitter
+  - picture
+  - alternativeText
+  - selected
+
+## Data modifications
+
+- Toggle status of gentleman property "selected"
+- Set status of every gentleman property "selected" as true
+- Delete gentleman
+
 # Components
 
 ## App
 
+- Contains the gentlemen list ✅
+
+- Contains the function toggleGentleman ✅
+- Contains the function selectEveryGentleman ✅
+- Contains the function deleteGentleman ✅
+
+- Sends the function toggleGentleman to Gentleman
+- Sends the function deleteGentleman to Gentleman
+- Sends the function selectEveryGentleman to Button
+
+- Sends the count of selected gentlemen to Info
+
 ## Info
 
-## Button
+- Receives the count of selected gentlemen from App
+- Displays the count of selected gentlemen
+
+## Button SelectAll
+
+- Receives the function selectEveryGentleman
 
 ## Gentleman
+
+- Receives the function toggleGentleman
+- Receives the function to deleteGentleman
+- Receives a Gentleman
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,135 @@
-function App() {
-  return <div></div>;
-}
+import { useState } from "react";
+import gentlemen from "../gentlemen";
+import Gentleman from "../types/types";
+
+const App = (): JSX.Element => {
+  const countOfSelectedGentlemen: number = gentlemen.filter(
+    (gentleman: Gentleman) => gentleman.selected
+  ).length;
+
+  const [, setGentlemen] = useState(gentlemen);
+
+  const selectEveryGentleman = () => {
+    gentlemen.forEach((gentleman) => {
+      gentleman.selected = true;
+    });
+
+    setGentlemen(gentlemen);
+  };
+
+  const toggleGentleman = (gentleman: Gentleman) => {
+    gentleman.selected = !gentleman.selected;
+  };
+
+  const deleteGentleman = (gentlemen: Gentleman[], { id }: Gentleman) => {
+    const indexOfGentleman = gentlemen.findIndex(
+      (gentleman) => gentleman.id === id
+    );
+
+    gentlemen.splice(indexOfGentleman, 1);
+
+    setGentlemen(gentlemen);
+  };
+
+  return (
+    <>
+      <header className="main-header">
+        <h1 className="main-title">The pointing gentlemen</h1>
+      </header>
+      <section className="controls">
+        <p className="info">0 gentlemen pointing at you</p>
+        <button className="button button--select">Select all</button>
+      </section>
+      <main className="main">
+        <ul className="gentlemen">
+          <li className="gentleman">
+            <div className="gentleman__avatar-container">
+              <img
+                className="gentleman__avatar"
+                src="img/fary.jpg"
+                alt="The Fary pointing at you"
+              />
+              <span className="gentleman__initial">F</span>
+            </div>
+            <div className="gentleman__data-container">
+              <h2 className="gentleman__name">The Fary</h2>
+              <ul className="gentleman__data-list">
+                <li className="gentleman__data">
+                  <span className="gentleman__data-label">Profession:</span>
+                  Influencer
+                </li>
+                <li className="gentleman__data">
+                  <span className="gentleman__data-label">Status:</span> RIP
+                </li>
+                <li className="gentleman__data">
+                  <span className="gentleman__data-label">Twitter:</span>{" "}
+                  Pending
+                </li>
+              </ul>
+            </div>
+            <i className="icon gentleman__icon fas fa-check"></i>
+            <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
+          </li>
+          <li className="gentleman">
+            <div className="gentleman__avatar-container">
+              <img
+                className="gentleman__avatar"
+                src="img/Julio.jpg"
+                alt="Julius Churchs pointing at you"
+              />
+              <span className="gentleman__initial">J</span>
+            </div>
+            <div className="gentleman__data-container">
+              <h2 className="gentleman__name">Julius Churchs</h2>
+              <ul className="gentleman__data-list">
+                <li className="gentleman__data">
+                  <span className="gentleman__data-label">Profession:</span>
+                  Youtuber
+                </li>
+                <li className="gentleman__data">
+                  <span className="gentleman__data-label">Status:</span> Alive
+                </li>
+                <li className="gentleman__data">
+                  <span className="gentleman__data-label">Twitter:</span>
+                  @julius_churchs
+                </li>
+              </ul>
+            </div>
+            <i className="icon gentleman__icon fas fa-check"></i>
+            <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
+          </li>
+          <li className="gentleman">
+            <div className="gentleman__avatar-container">
+              <img
+                className="gentleman__avatar"
+                src="img/bertin.jpg"
+                alt="Bertin Osbourne pointing at you"
+              />
+              <span className="gentleman__initial">B</span>
+            </div>
+            <div className="gentleman__data-container">
+              <h2 className="gentleman__name">Bertin Osbourne</h2>
+              <ul className="gentleman__data-list">
+                <li className="gentleman__data">
+                  <span className="gentleman__data-label">Profession:</span>
+                  Java developer
+                </li>
+                <li className="gentleman__data">
+                  <span className="gentleman__data-label">Status:</span> Alive
+                </li>
+                <li className="gentleman__data">
+                  <span className="gentleman__data-label">Twitter:</span>{" "}
+                  @osbourne
+                </li>
+              </ul>
+            </div>
+            <i className="icon gentleman__icon fas fa-check"></i>
+            <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
+          </li>
+        </ul>
+      </main>
+    </>
+  );
+};
 
 export default App;
